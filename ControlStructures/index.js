43cf33d83400628c09aefe96cee67cc88e6585a1
Avanishyadav9035 <yadavavnish683@gmail.com>
2025-09-05 13:50:03 +0530

    first commit

---
+++ ControlStructures/index.js
@@ -0,0 +1,34 @@
+//  let age = Number(prompt("Enter your age"))
+ //  let age = 14
+// if(age >= 1 && age <= 12)
+// {
+//     console.log("Kid")
+// }
+// else if(age >= 13 && age <= 19)
+// {
+//     console.log("Teenager")
+// }
+// else if(age >= 20 && age <= 64)
+// {
+//     console.log("Adult")
+// }
+// else if(age >= 64)
+// {
+//     console.log("Senior Citizen")
+// }
+// else
+// {
+//     console.log("Senior Citizen")
+// }
+
+
+let num = 21
+
+if(num<0){
+    console.log("-ve")
+}
+else if(num==0){
+    console.log("zero")
+} else{
+    console.log("+ve")
+}
