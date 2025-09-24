4271e47b9880822df1d2bb6e38cd6fa07773200f
Avanishyadav9035 <yadavavnish683@gmail.com>
2025-09-24 16:16:56 +0530

    Async update

--- AsyncJS/index.js
+++ AsyncJS/index.js
@@ -139,8 +139,9 @@
 // Event Loop moves tasks to stack.
 
 
-console.log("Start")
-setTimeout(()=>console.log("Timeout"),0);
-Promise.resolve().then(()=>console.log("Promise"));
+// console.log("Start")
+// setTimeout(()=>console.log("Timeout"),0);
+// Promise.resolve().then(()=>console.log("Promise"));
 
-console.log("End")
+// console.log("End")
+// Promise (microtask) runs before Timeout (macro task).
